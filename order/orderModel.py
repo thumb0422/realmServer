f9a5952913b170d184713be83e3aa5cd78f19fbf
liuchunhua <thumb0422@163.com>
2017-10-31 20:48:43 +0800

    update

--- order/orderModel.py
+++ order/orderModel.py
@@ -31,8 +31,11 @@ class OrderDetail(db.Model):
     states = db.Column(db.CHAR(1))
     createDate = db.Column(db.DateTime)
     updateDate = db.Column(db.DateTime)
+    # 用于外键的字段
     orderId = db.Column(db.String(15),db.ForeignKey('TM_OrderMain.orderId'))
-    orderMain = db.relationship('TM_OrderDetail',backref='TM_OrderMain',lazy='dynamic')
+    # 外键对象，不会生成数据库实际字段
+    # backref指反向引用，也就是外键Category通过backref(OrderMain_Detail)查询Post
+    orderMain = db.relationship('TM_OrderDetail',backref=db.backref('OrderMain_Detail',lazy='dynamic'))
 
     def __init__(self,detailId,orderId,states):
         self.detailId = detailId
