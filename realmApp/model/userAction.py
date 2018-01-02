084714c30f4cc16cfacd6a7246d6b50b43631e90
liuchunhua <chunhua1@eastmoney.com>
2018-01-02 17:49:47 +0800

    add

--- realmApp/model/userAction.py
+++ realmApp/model/userAction.py
@@ -1,6 +1,11 @@
 # coding: utf-8
+from sqlalchemy.ext.declarative import DeclarativeMeta
 from realmApp.model import *
 from realmApp.model.model import *
+from realmApp.utility import *
+from realmApp.utility.Response import *
+from flask import jsonify
+from realmApp.utility.alchemyEncoder import *
 
 '''SQL'''
 # sql = text("select * from TM_Address")
@@ -12,5 +17,46 @@ from realmApp.model.model import *
 # session.close()
 
 '''ORM'''
-res = session.query(TMAddres).filter(text("addressId=1")).one()
-print (res.addressId,res.userId,res.address)
+# res = session.query(TMAddres).filter(text("addressId=1")).one()
+# print (res.addressId,res.userId,res.address)
+
+
+class UserView:
+
+    @classmethod
+    def queryUsersORM(cls):
+        query = session.query(TMUSER)
+        print(query)
+        users = query.all()
+        user = users[0]
+        print(user.userId,user.userCode,user.userName)
+
+    @classmethod
+    def queryUsersSQL(cls):
+        sql = text("select * from TM_USER")
+        res = session.execute(sql).fetchall()
+        resultArray = rowToArray(res)
+        session.close()
+        # sqlResultStr = json.dumps(resultArray, cls=AlchemyEncoder)
+        # print(sqlResultStr)
+        resultDic = {"status": 0, 'message': '查询成功', "count": resultArray.__len__(), "datas": resultArray}
+        resultJson = json.dumps(resultDic, cls=AlchemyEncoder,indent=4,sort_keys=True)
+        return resultJson
+
+
+    @classmethod
+    def queryUsersView(cls):
+        pass;
+
+    @classmethod
+    def saveUsers(cls):
+        user = TMUSER()
+        user.userCode = getModelKey('UR')
+        user.userName = 'TEST1'
+        user.email = 'thumb0422@163.com'
+        session.add(user)
+        session.commit()
+
+# UserView.saveUsers()
+# UserView.queryUsers()
+UserView.queryUsersSQL()
---
+++ realmApp/utility/alchemyEncoder.py
@@ -0,0 +1,53 @@
+# coding: utf-8
+'''对sqlalchemy查询出来的数据进行转换'''
+from sqlalchemy.ext.declarative import DeclarativeMeta
+import json,datetime,decimal
+
+def new_alchemy_encoder():
+    _visited_objs = []
+    class AlchemyEncoder(json.JSONEncoder):
+        def default(self, obj):
+            if isinstance(obj.__class__, DeclarativeMeta):
+                # don't re-visit self
+                if obj in _visited_objs:
+                    return None
+                _visited_objs.append(obj)
+
+                # an SQLAlchemy class
+                fields = {}
+                for field in [x for x in dir(obj) if not x.startswith('_') and x != 'metadata']:
+                    fields[field] = obj.__getattribute__(field)
+                # a json-encodable dict
+                return fields
+
+            return json.JSONEncoder.default(self, obj)
+
+    return AlchemyEncoder
+
+
+
+class AlchemyEncoder(json.JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj.__class__, DeclarativeMeta):
+            # an SQLAlchemy class
+            fields = {}
+            for field in [x for x in dir(obj) if not x.startswith('_') and x != 'metadata']:
+                data = obj.__getattribute__(field)
+                try:
+                    json.dumps(data)  # this will fail on non-encodable values, like other classes
+                    fields[field] = data
+                except TypeError:  # 添加了对datetime的处理
+                    if isinstance(data, datetime.datetime):
+                        fields[field] = data.isoformat()
+                    elif isinstance(data, datetime.date):
+                        fields[field] = data.isoformat()
+                    elif isinstance(data, datetime.timedelta):
+                        fields[field] = (datetime.datetime.min + data).time().isoformat()
+                    elif isinstance(obj, decimal.Decimal):
+                            return float(obj)
+                    else:
+                        fields[field] = None
+            # a json-encodable dict
+            return fields
+
+        return json.JSONEncoder.default(self, obj)
