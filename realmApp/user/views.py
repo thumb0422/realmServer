8ebbb3b2599bb3ed94749f8141eef8490b3844e9
liuchunhua <chunhua1@eastmoney.com>
2018-01-16 14:14:05 +0800

    update

--- realmApp/user/views.py
+++ realmApp/user/views.py
@@ -9,62 +9,41 @@ from realmApp.model.userAction import *
 def index():
     return 'Hello World I am user'
 
-'''getUser'''
+'''查询单个用户信息'''
 @user.route('/F1001',methods = ['POST'])
 def getUser():
-    if request.json is not None:
-        jsonDic = converJsonToDic(request.json)
-        if 'userCode' in jsonDic.keys():
-            userCode = jsonDic['userCode'] if jsonDic['userCode'] is not None else ""
-        else:
-            userCode = ''
-        return UserView.queryUsersSQL(userCode)
-    return DataResopnse(0, '查询成功', []).toJson()
-
-'''register'''
-@user.route('/F1005',methods = ['POST'])
-def register():
-    '''
-    用户注册事件
-    :return:
-    '''
     if not request.json :
         return  DataResopnse(-1, '非json格式', []).toJson()
     data = request.data.decode('utf-8')
     requestDic = json.loads(data)
+    if 'userCode' in requestDic.keys():
+        userCode = requestDic['userCode'] if requestDic['userCode'] is not None else ""
+    else:
+        userCode = ''
+    return UserView.queryUsersSQL(userCode)
 
-    # requestStr = json.dumps(request.json)
-    # requestDic = eval(requestStr)
+'''用户注册'''
+@user.route('/F1005',methods = ['POST'])
+def register():
+    if not request.json :
+        return  DataResopnse(-1, '非json格式', []).toJson()
+    data = request.data.decode('utf-8')
+    requestDic = json.loads(data)
     result = UserView.saveUsers(**requestDic)
     return result
 
-'''update用户资料'''
+'''更新用户资料'''
 @user.route('/F1006',methods = ['POST'])
 def updateregister():
-    '''
-    更新用户状态
-    TODO:
-    '''
     if not request.json :
         return  DataResopnse(-1, '非json格式', []).toJson()
     data = request.data.decode('utf-8')
     requestDic = json.loads(data)
     pass
 
-'''login'''
+'''用户登录'''
 @user.route('/F1007',methods = ['POST'])
 def login():
-    '''
-    用户登录
-    :return:
-    '''
-    '''
-    parser = reqparse.RequestParser()
-    parser.add_argument('userCode', type=str, help='XXXXXX')
-    parser.add_argument('userPwd', type=str)
-    args = parser.parse_args()
-    '''
-
     if not request.json :
         return  DataResopnse(-1, '非json格式', []).toJson()
     data = request.data.decode('utf-8')
@@ -76,20 +55,16 @@ def login():
     else:
         return DataResopnse(-1, '该用户未注册', []).toJson()
 
-'''logout'''
+'''用户登出'''
 @user.route('/F1008',methods = ['POST'])
 def logout():
-    '''
-    用户登出
-    :return:
-    '''
     if not request.json :
         return  DataResopnse(-1, '非json格式', []).toJson()
     data = request.data.decode('utf-8')
     requestDic = json.loads(data)
     return UserView.userLogOut(**requestDic)
 
-'''deleteUser'''
+'''用户注销'''
 @user.route('/F1009',methods = ['POST'])
 def deleteUser():
     if not request.json :
