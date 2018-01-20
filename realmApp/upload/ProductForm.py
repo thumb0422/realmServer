31c1ccfd68ed4dc731f9ff72e53064b7101efe54
liuchunhua <thumb0422@163.com>
2018-01-20 22:54:38 +0800

    update

--- realmApp/upload/ProductForm.py
+++ realmApp/upload/ProductForm.py
@@ -4,15 +4,14 @@ from wtforms import StringField, SubmitField,SelectField
 from wtforms.validators import Required,DataRequired
 from realmApp.model.staticAction import *
 
-class ProductForm(FlaskForm):
+class ProductTypeForm(FlaskForm):
     version = StringField(u'版本名称', validators=[DataRequired('')])
-    # project = StringField(u'项目', validators=[DataRequired()])
-    # style   = StringField(u'款式', validators=[DataRequired()])
-    # model   = StringField(u'型号', validators=[DataRequired()])
-    # level   = StringField(u'单/双层', validators=[DataRequired()])
-
     project = SelectField(u'项目',   choices=StaticDataView.queryProdcuctProjectView(), validators=[DataRequired()])  # key 不能是int
     style   = SelectField(u'款式',   choices=StaticDataView.queryProdcuctStyleView(),   validators=[DataRequired()])  # key 不能是int
     model   = SelectField(u'型号',   choices=StaticDataView.queryProdcuctModelView(),   validators=[DataRequired()])  # key 不能是int
     level   = SelectField(u'单/双层',choices=StaticDataView.queryProdcuctLevelView(),    validators=[DataRequired()])  # key 不能是int
     submit  = SubmitField(u'提交')
+
+
+class ProductInfoForm(FlaskForm):
+    pass
--- realmApp/upload/uView.py
+++ realmApp/upload/uView.py
@@ -5,20 +5,21 @@ from .uForm import *
 from .ProductForm import *
 from realmApp.model.productAction import *
 
+'''产品 信息展示'''
 @upload.route('/index',methods=['GET', 'POST'])
 def index():
-    name = None
-    form = ProductForm()
-    # 如果提交的数据验证通过，则返回True
-    if form.validate_on_submit():
-        name = form.name.data
-        form.name.data = ''
-    return render_template('productIndex.html', form=form, name=name)
+    pass
 
-@upload.route('/add',methods=['GET', 'POST'])
-def add():
+'''TM_Product 产品信息 表数据维护'''
+@upload.route('/index/infoAdd',methods=['GET', 'POST'])
+def infoAdd():
+    pass
+
+'''TM_ProductType 产品类型 表数据维护'''
+@upload.route('/index/typeAdd',methods=['GET', 'POST'])
+def typeAdd():
     name = None
-    form = ProductForm()
+    form = ProductTypeForm()
     # 如果提交的数据验证通过，则返回True
     if form.validate_on_submit():
         version = form.version.data
