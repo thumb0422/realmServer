b5bf3775421ae1ad8c60accb57fd70924dd95d6b
liuchunhua <chunhua1@eastmoney.com>
2018-01-25 10:41:20 +0800

    update
    js 都采用本地  加速

--- realmApp/upload/ProductForm.py
+++ realmApp/upload/ProductForm.py
@@ -19,7 +19,7 @@ class ProductInfoForm(FlaskForm):
     productCode = StringField(u'产品代码', validators=[DataRequired(''),Length(min=4,max=4,message=u'必须等于4字符！')])
     productName = StringField(u'产品名称', validators=[DataRequired(''),Length(min=4,max=200,message=u'必须介于4-200字符！')])
     typeCode    = SelectField(u'产品类型',choices=StaticDataView.queryProdcuctTypeView(), validators=[DataRequired()])  # key 不能是int
-    productImg  = FileField(u'图片',validators=[FileRequired(),FileAllowed(['png','jpg','jpeg'],u'必须为图片类型')])
+    productImg  = FileField(u'选择图片',validators=[FileRequired(u'请选择图片'),FileAllowed(['png','jpg','jpeg'],u'必须为图片类型')])
     costPrice   = FloatField(u'成本价格', validators=[DataRequired(u'必须为数字类型')])
     salePrice   = FloatField(u'销售价格',validators=[DataRequired(u'必须为数字类型')])
     submit = SubmitField(u'提交')
